017a235db5456a500a5c05150187cfd180127129
bhnone <>
2024-08-31 01:28:38 +0900

    nothing here

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editng&nbsp;
+          Get started by eng&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
